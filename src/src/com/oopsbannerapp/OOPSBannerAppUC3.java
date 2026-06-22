7e1c8d2e4e463a99369586fc790beb73b2c38d1a
Raju Madhabtula <nanivivo320@gmail.com>
2026-06-22 11:44:47 +0530

    UC3: Render OOPS banner using String.join()

---
+++ src/src/com/oopsbannerapp/OOPSBannerAppUC3.java
@@ -0,0 +1,19 @@
+package com.oopsbannerapp;
+
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+
+        String banner = String.join("\n",
+            " ***     ***    ******    ***** ",
+            "** **   ** **   **   **  **     ",
+            "** **   ** **   **   **  **     ",
+            "** **   ** **   ******     *****",
+            "** **   ** **   **             **",
+            "** **   ** **   **             **",
+            " ***     ***    **        ***** "
+        );
+
+        System.out.println(banner);
+    }
+}
